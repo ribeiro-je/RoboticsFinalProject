4422133362affc2c5c1fa292531fae4e9c19cc23
margarita26 <gubanova.marg@yandex.com>
2020-11-29 12:00:11 -0500

    feat: control program reads robot's output

--- mbot/control_program/control_program.ino
+++ mbot/control_program/control_program.ino
@@ -5,16 +5,16 @@
 #include <SPI.h>
 #include <Pixy2.h>
 #include <Pixy2I2C.h>
-
- #include "grid.hh"
- #include "viz.hh"
- #include "pose.hh"
+#include <SoftwareSerial.h>
 
 MeEncoderOnBoard Encoder_1(SLOT1);
 MeEncoderOnBoard Encoder_2(SLOT2);
 MeRGBLed rgbled_0(0, 12);
-MeUltrasonicSensor ultrasonic_8(8);
+MeUltrasonicSensor ultrasonic_8(9);
+
+MeBuzzer buzzer;
 Pixy2I2C pixy;
+
 
 void isr_process_encoder1(void)
 {
@@ -80,7 +80,7 @@ void move(int direction, int speed)
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.print("Starting...\n");
   rgbled_0.setpin(44);
   rgbled_0.fillPixelsBak(0, 2, 1);
@@ -89,12 +89,13 @@ void setup()
   TCCR2A = _BV(WGM21) | _BV(WGM20);
   TCCR2B = _BV(CS21);
 
+  buzzer.setpin(45);
+
   attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
   attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
 
   pixy.init();
 
-  //viz_run(0, NULL);
 }
 
 void _loop()
@@ -103,7 +104,7 @@ void _loop()
   Encoder_2.loop();
 }
 
-bool is_object_detected(int sig_num)
+bool is_object_detected(int sign)
 {
   pixy.ccc.getBlocks();
 
@@ -111,9 +112,8 @@ bool is_object_detected(int sig_num)
   {
     for (int i = 0; i < pixy.ccc.numBlocks; i++)
     {
-      if (pixy.ccc.blocks[i].m_signature == sig_num)
+      if (pixy.ccc.blocks[i].m_signature == sign)
       {
-        Serial.println("Detected");
         return true;
       }
     }
@@ -121,44 +121,80 @@ bool is_object_detected(int sig_num)
   return false;
 }
 
-void loop()
-{
-  _loop();
-
+void process_data() {
+  Serial.print("d");
+  Serial.println(ultrasonic_8.distanceCm());
+  Serial.print("1");
   bool is_1_detected = is_object_detected(1);
+  Serial.println(is_1_detected);
+  Serial.print("2");
+  bool is_2_detected = is_object_detected(2);
+  Serial.println(is_2_detected);
+  Serial.print("3");
+  bool is_3_detected = is_object_detected(3);
+  Serial.println(is_3_detected);
+  Serial.print("4");
+  bool is_4_detected = is_object_detected(4);
+  Serial.println(is_4_detected);
 
   if (is_1_detected)
   {
     rgbled_0.setColor(0, 0, 93, 255);
     rgbled_0.show();
-    Encoder_1.setTarPWM(0);
-    Encoder_2.setTarPWM(0);
-    _delay(10);
+    //    buzzer.tone(100, 1 * 1000);
+    //    Encoder_1.setTarPWM(0);
+    //    Encoder_2.setTarPWM(0);
+    //    _delay(5);
   }
-
-  Serial.println(ultrasonic_8.distanceCm());
-  if (ultrasonic_8.distanceCm() < 15)
+  else if (is_2_detected)
   {
     rgbled_0.setColor(0, 255, 0, 0);
     rgbled_0.show();
-
-    move(3, 50 / 100.0 * 255);
-    _delay(1);
-    move(3, 0);
+    //    buzzer.tone(300, 1 * 1000);
+    //    Encoder_1.setTarPWM(0);
+    //    Encoder_2.setTarPWM(0);
+    //    _delay(5);
   }
-  else
+  else if (is_3_detected)
   {
-    rgbled_0.setColor(0, 174, 255, 0);
+    rgbled_0.setColor(0, 146, 73, 0);
     rgbled_0.show();
-    move(1, 70 / 100.0 * 255);
+    //    buzzer.tone(500, 1 * 1000);
+    //    Encoder_1.setTarPWM(0);
+    //    Encoder_2.setTarPWM(0);
+    //    _delay(5);
   }
-
-  // up to 2.5 meters away? maybe 100 would work better inside though...
-  if (ultrasonic_8.distanceCm() < 250)
+  else if (is_4_detected)
   {
-     Pose p = pose(robot->pos_x, robot->pos_y, robot->pos_t); // figure out of to get pose
-     float distance = ultrasonic_8.distanceCm() / 100; this should be meters I think? so / 100? test to confirm tho
-     float angle = 0.0; i think 0 bc always straight ahead
-     grid_apply_hit(distance, angle, pose);
+    rgbled_0.setColor(0, 72, 243, 0);
+    rgbled_0.show();
+    //    buzzer.(700, 1 * 1000);
+    //    Encoder_1.setTarPWM(0);
+    //    Encoder_2.setTarPWM(0);
+    //    _delay(5);
   }
+  else {
+    rgbled_0.setColor(0, 0, 0, 0);
+    rgbled_0.show();
+  }
+}
+
+void loop()
+{
+  //  _loop();
+  //
+  //  if (ultrasonic_8.distanceCm() < 15)
+  //  {
+  //    move(3, 50 / 100.0 * 255);
+  //    _delay(1);
+  //    move(2, 0);
+  //  }
+  //  else
+  //  {
+  //    move(1, 70 / 100.0 * 255);
+  //  }
+
+  process_data();
+
+  delay(500);
 }
